a96a526599d1ea5a208cd413186237b08d28119e
Pierre-Yves Ritschard <pyr@spootnik.org>
2015-07-31 14:56:15 +0200

    add project

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject om-sandbox "0.3.0"
+  :description "playground for om projects"
+  :url "https://github.com/pyr/om-sandbox"
+  :license {:name "MIT License"}
+  :plugins [[lein-sub "0.3.0"]]
+  :sub ["minesweeper" "jobs" "cuteworld"]
+  :dependencies [[org.clojure/clojure "1.7.0"]])
